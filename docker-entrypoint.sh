ba0ca1c6078d6f77c13f84bf42d31813158749ed
cross <aiyinsitan@outlook.com>
2017-04-11 19:44:45 +0800

    add: aof to be always.

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -9,10 +9,19 @@ sed -i "s/port 6379/port $CLIENTPORT/g" /etc/redis.conf
 sed -i "s/# requirepass foobared/requirepass $REQUIREPASS/g" /etc/redis.conf
 sed -i "s/# masterauth <master-password>/masterauth $REQUIREPASS/g" /etc/redis.conf
 
-## slaveof <masterip> <masterport>
+## slaveof <masterip> <masterport> => slaveof $MASTERHOST $MASTERPORT
 if [ "$MASTERPORT" != "" ];then
 	sed -i "s/# slaveof <masterip> <masterport>/slaveof $MASTERHOST $MASTERPORT/g" /etc/redis.conf
 fi
+
+## appendfsync everysec => appendfsync $APPENDFSYNC
+if [ "$APPENDFSYNC" != "" ];then
+	sed -i "s/appendonly no/appendonly yes/g" /etc/redis.conf
+	sed -i "s/appendfsync everysec/appendfsync $APPENDFSYNC/g" /etc/redis.conf
+fi
+
+
+
 
 
 # first arg is `-f` or `--some-option`
